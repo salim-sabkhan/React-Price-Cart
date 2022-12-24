a256fcd0f9d345e9fc38f7473613701d702e1e65
salim-sabkhan <sabkhansalim@gmail.com>
2022-12-24 17:45:34 +0530

    first commit

---
+++ src/Salim.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+ import Content from "./Content";
+ import Feature from "./Feature";
+ import Footer from "./Footer";
+ import NavBar from "./NavBar";
+ import Service from "./Service";
+
+
+
+const Salim = () => {
+  return(
+    <div>
+       <NavBar/> 
+       <Content/>
+       <Feature/>
+       <Service/>
+       <Footer/> 
+     </div>
+   ); 
+ };
+
+ export default Salim;
+
+
+// import React from "react";
+
+// const Salim=()=>{
+//   return(
+//    <div>
+//   <h4>Hi Salim</h4>
+//   <p>Hello World</p>
+// </div>
+//   );
+// };
+
+// export default Salim;
